818c6100caa521750c0d5027b1c2b88b8ab203bb
ayushkumar <ge95xod@mytum.de>
2024-02-08 21:09:58 +0100

    Removed the unnecessay column transformers and made inidividual pipelines for the models.

--- notebooks/laipred/model_factory.py
+++ notebooks/laipred/model_factory.py
@@ -1,8 +1,6 @@
 from sklearn import linear_model
-from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPRegressor
 
 
@@ -15,12 +13,6 @@ class ModelFactory:
     def create_linear_regression_orig(cls):
         return Pipeline(
             [
-                (
-                    "project_scale",
-                    ColumnTransformer(
-                        [("scaler", StandardScaler(), cls.COLS_USED_BY_ORIGINAL_MODELS)]
-                    ),
-                ),
                 ("model", linear_model.LinearRegression()),
             ]
         )
@@ -29,25 +21,29 @@ class ModelFactory:
     def create_random_forest_orig(cls):
         return Pipeline(
             [
-                (
-                    "project_scale",
-                    ColumnTransformer(
-                        [("scaler", StandardScaler(), cls.COLS_USED_BY_ORIGINAL_MODELS)]
-                    ),
-                ),
                 ("model", RandomForestRegressor(n_estimators=100)),
             ]
         )
 
     @classmethod
     def pure_random_forest(cls):
-        return RandomForestRegressor(n_estimators=100, max_depth=100, random_state=42)
+                return Pipeline(
+            [
+                ("model", RandomForestRegressor(n_estimators=100, max_depth=100, random_state=42)),
+            ]
+        )
+
 
     @classmethod
     def pure_mlp(cls):
-        return MLPRegressor(
+        return Pipeline(
+            [
+                ("model",MLPRegressor(
             hidden_layer_sizes=(1000, 100, 10),
             max_iter=1000,
             random_state=42,
-            early_stopping=True,
+            early_stopping=True,)),
+            ]
         )
+
+
