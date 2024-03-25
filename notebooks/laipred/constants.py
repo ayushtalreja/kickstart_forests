20ebb4a93eee812401956080ab3764ad514b0dec
Valentina Izrailevitch <izrailevitch@cip.ifi.lmu.de>
2024-03-25 17:49:29 +0100

    add constants.py

---
+++ notebooks/laipred/constants.py
@@ -0,0 +1,7 @@
+MLFLOW_TRACKING_URL = "https://team1-mlflow-0d6bb1b-k4avrf3gmq-ey.a.run.app/"
+MLFLOW_USERNAME = 'admin'
+MLFLOW_PASSWORD = 'HDtZMsBD9pkSnUN1'
+MLFLOW_EXPERIMENT = "laipred"
+
+LAKEFS_REPOSITORY = "ai-kickstart"
+LAKEFS_BRANCH = "main"
